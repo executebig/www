b1ad035b63c42ec5b3ad06897f97dac4b9aa8f38
Mingjie Jiang <jiang@mingjie.dev>
2020-08-16 15:29:53 -0400

    Remove templates

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,78 +10,78 @@ const wrapper = promise =>
     return result
   })
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions
 
-  const result = await wrapper(
-    graphql(`
-      {
-        prismic {
-          project {
-            edges {
-              node {
-                project_title
-                project_preview_description
-                project_preview_thumbnail
-                project_category
-                project_post_date
-                _meta {
-                  uid
-                }
-              }
-            }
-          }
-          post {
-            edges {
-              node {
-                post_title
-                post_hero_image
-                post_hero_annotation
-                post_date
-                post_category
-                post_body
-                post_preview_description
-                post_author
-                _meta {
-                  uid
-                }
-              }
-            }
-          }
-        }
-      }
-    `)
-  )
+//   const result = await wrapper(
+//     graphql(`
+//       {
+//         prismic {
+//           project {
+//             edges {
+//               node {
+//                 project_title
+//                 project_preview_description
+//                 project_preview_thumbnail
+//                 project_category
+//                 project_post_date
+//                 _meta {
+//                   uid
+//                 }
+//               }
+//             }
+//           }
+//           post {
+//             edges {
+//               node {
+//                 post_title
+//                 post_hero_image
+//                 post_hero_annotation
+//                 post_date
+//                 post_category
+//                 post_body
+//                 post_preview_description
+//                 post_author
+//                 _meta {
+//                   uid
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     `)
+//   )
 
-  const projectsList = result.data.prismic.project.edges
-  const postsList = result.data.prismic.post.edges
+//   const projectsList = result.data.prismic.project.edges
+//   const postsList = result.data.prismic.post.edges
 
-  const projectTemplate = require.resolve("./src/templates/project.jsx")
-  const postTemplate = require.resolve("./src/templates/post.jsx")
+//   const projectTemplate = require.resolve("./src/templates/project.jsx")
+//   const postTemplate = require.resolve("./src/templates/post.jsx")
 
-  //   projectsList.forEach(edge => {
-  //     // The uid you assigned in Prismic is the slug!
-  //     createPage({
-  //       type: "Project",
-  //       match: "/work/:uid",
-  //       path: `/work/${edge.node._meta.uid}`,
-  //       component: projectTemplate,
-  //       context: {
-  //         // Pass the unique ID (uid) through context so the template can filter by it
-  //         uid: edge.node._meta.uid,
-  //       },
-  //     })
-  //   })
+//   //   projectsList.forEach(edge => {
+//   //     // The uid you assigned in Prismic is the slug!
+//   //     createPage({
+//   //       type: "Project",
+//   //       match: "/work/:uid",
+//   //       path: `/work/${edge.node._meta.uid}`,
+//   //       component: projectTemplate,
+//   //       context: {
+//   //         // Pass the unique ID (uid) through context so the template can filter by it
+//   //         uid: edge.node._meta.uid,
+//   //       },
+//   //     })
+//   //   })
 
-  //   postsList.forEach(edge => {
-  //     createPage({
-  //       type: "Project",
-  //       match: "/blog/:uid",
-  //       path: `/blog/${edge.node._meta.uid}`,
-  //       component: postTemplate,
-  //       context: {
-  //         uid: edge.node._meta.uid,
-  //       },
-  //     })
-  //   })
-}
+//   //   postsList.forEach(edge => {
+//   //     createPage({
+//   //       type: "Project",
+//   //       match: "/blog/:uid",
+//   //       path: `/blog/${edge.node._meta.uid}`,
+//   //       component: postTemplate,
+//   //       context: {
+//   //         uid: edge.node._meta.uid,
+//   //       },
+//   //     })
+//   //   })
+// }
