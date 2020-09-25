4cf72521bf9f2cb30b1f636c7c7aa3cd2c7c7044
Mingjie Jiang <jiang@mingjie.dev>
2020-09-25 12:20:56 -0400

    Remove purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,33 +1,5 @@
 module.exports = {
   plugins: {
-    "@fullhuman/postcss-purgecss": {
-      content: ["layouts/**/*.html"],
-      whitelist: [
-        "highlight",
-        "pre",
-        "code",
-        "content",
-        "h3",
-        "h4",
-        "ul",
-        "li",
-        "table",
-        "blockquote",
-        "page",
-        "page-home",
-        "lock-scroll",
-        "open",
-      ],
-      whitelistPatternsChildren: [
-        /^hamburger(-)?/,
-        /^main-menu(-)?/,
-        /^content/,
-        /^sponsors/,
-        /^(spinner)(-)?/,
-        /^donationContainer/,
-        /^carousel/,
-      ],
-    },
     autoprefixer: {},
     cssnano: { preset: "default" },
   },
