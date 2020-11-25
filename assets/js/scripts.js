fad7d3454c1cf60a9552d97ae65ef12fa5dd3b72
Mingjie Jiang <jiang@mingjie.dev>
2020-11-25 16:30:05 -0500

    Bundle js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,6 +5,30 @@ var searchToggle = document.querySelectorAll(".searchToggle");
 var searchContainer = document.querySelector("#search-container");
 var cancelTrigger = document.querySelector("#cancelSearch");
 
+var client = algoliasearch("5UQN5RC9UK", "19f815aefb3ec262ec3e715b0bbe4e85");
+var index = client.initIndex(searchIndex);
+
+autocomplete("#search-input", { hint: false, appendTo: "#searchResults" }, [
+  {
+    source: autocomplete.sources.hits(index, { hitsPerPage: 5 }),
+    displayKey: "title",
+    templates: {
+      suggestion: function (suggestion) {
+        return suggestion._highlightResult.title.value;
+      },
+    },
+  },
+])
+  .on("autocomplete:shown", function (event, suggestion, dataset, context) {
+    document.getElementById("cancelSearch").style.visibility = "hidden";
+  })
+  .on("autocomplete:closed", function (event, suggestion, dataset, context) {
+    document.getElementById("cancelSearch").style.visibility = "visible";
+  })
+  .on("autocomplete:selected", function (event, suggestion, dataset, context) {
+    window.location.href = suggestion.permalink;
+  });
+
 menuTrigger.onclick = function () {
   menuContainer.classList.toggle("open");
   menuTrigger.classList.toggle("is-active");
