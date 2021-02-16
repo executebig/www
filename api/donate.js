18c5ae808d88cd3644da0eabce0b2d8830a7bfa2
Mingjie Jiang <jiang@mingjie.dev>
2021-02-16 15:21:10 -0500

    Rebuild donation security

--- api/donate.js
+++ api/donate.js
@@ -1,24 +1,33 @@
 require("dotenv").config();
+const { verify } = require("hcaptcha");
 const stripe = require("stripe")(process.env.STRIPE_API_KEY);
+const secret = process.env.HCAPTCHA_SECRET;
+export default (req, res) => {
+  const amount = req.body.amount;
+  const host = req.body.host;
 
-export default async (req, res) => {
-  const amount = req.body.amount
-  const host = req.body.host
+  verify(secret, req.body.token)
+  .then(async (data) => {
+    if (data.success) {
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: [
+          {
+            name: "Donate to Execute Big",
+            description: "Your contribution is tax-deductible.",
+            amount: amount * 100, // Cents
+            currency: "usd",
+            quantity: 1,
+          },
+        ],
+        success_url: host + "/donate/success/?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url: host + "/donate/",
+      });
 
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items: [
-      {
-        name: "Donate to Execute Big",
-        description: "Your contribution is tax-deductible.",
-        amount: amount * 100, // Cents
-        currency: "usd",
-        quantity: 1,
-      },
-    ],
-    success_url: host + "/donate/success/?session_id={CHECKOUT_SESSION_ID}",
-    cancel_url: host + "/donate/",
-  });
-
-  return res.status(200).json(session);
+      return res.status(200).json(session);
+    } else {
+      return res.status(500);
+    }
+  })
+  .catch(console.error);
 };
--- assets/js/pages/donate.js
+++ assets/js/pages/donate.js
@@ -1,99 +1,68 @@
 const donateButton = document.getElementById("donateButton");
-const coverFeeButton = document.getElementById("coverFee");
 const amountField = document.getElementById("amount");
 const feeAmount = document.getElementById("fee");
-const donateAmount = document.getElementById("total");
-const dollarSign = document.getElementById("dollarSign");
 
-const validate = (value) => {
-  if (isNaN(value) || value < 0.5 || value > 999999.99) {
-    if (value !== "") {
-      amountField.classList.add("danger");
-      dollarSign.classList.add("danger");
-    } else {
-      amountField.classList.remove("danger");
-      dollarSign.classList.remove("danger");
-    }
+const roundTwoDigits = (v) => +(Math.round(v + "e+2") + "e-2");
+const calcFee = (v) => v == 0? 0 : roundTwoDigits((v + 0.3) / (1 - 0.022) - v);
+const validate = (value) => !isNaN(value) && value >= 0.5 && value <= 999999.99;
+const normalizeAmount = (v) => (v < 0.5 ? 0 : v > 999999.99 ? 999999.99 : v);
 
-    donateButton.classList.add("disabled");
-    coverFeeButton.classList.add("disabled");
+amountField.addEventListener("keyup", () => {
+  feeAmount.innerText = calcFee(normalizeAmount(Number(amountField.value)));
+});
 
-    donateAmount.innerText = "0.00";
-    feeAmount.innerText = "0.00";
+amountField.addEventListener("change", () => {
+  amountField.value = normalizeAmount(Number(amountField.value)).toFixed(2);
+});
 
-    return false;
+donateButton.onclick = (event) => {
+  event.preventDefault();
+  const amount = normalizeAmount(Number(amountField.value));
+  if (validate(amount)) {
+    hcaptcha.execute();
   } else {
-    amountField.classList.remove("danger");
-    dollarSign.classList.remove("danger");
-    donateButton.classList.remove("disabled");
-    coverFeeButton.classList.remove("disabled");
-
-    v = parseFloat(value);
-
-    donateAmount.innerText = v.toFixed(2);
-    feeAmount.innerText = ((v + 0.3) / (1 - 0.022) - v).toFixed(2);
-
-    return true;
+    alert("Invalid amount!");
   }
 };
 
-amountField.addEventListener("keyup", function () {
-  validate(this.value);
-});
+var launchDonate = (token) => {
+  const coverFee = document.getElementById("coverFee").checked;
+  const rawAmount = normalizeAmount(Number(amountField.value));
+  const amount = coverFee ? rawAmount + calcFee(rawAmount) : rawAmount;
 
-amountField.addEventListener("change", function () {
-  this.value = parseFloat(this.value).toFixed(2);
-  validate(this.value);
-});
+  console.log(coverFee);
+  console.log(rawAmount);
+  console.log(amount);
 
-donateButton.addEventListener("click", function () {
-  launchDonate(this, amountField.value);
-});
+  donateButton.innerHTML =
+    '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>&ensp;Loading...';
+  donateButton.setAttribute("disabled", true);
+  donateButton.classList.add("disabled");
 
-coverFeeButton.addEventListener("click", function () {
-  launchDonate(
-    this,
-    parseFloat(amountField.value) + parseFloat(feeAmount.innerText)
-  );
-});
-
-const launchDonate = (el, amount) => {
-  grecaptcha.ready(function () {
-    grecaptcha
-      .execute(recaptchaKey, { action: "submit" })
-      .then(function (token) {
-        el.innerHTML =
-          '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>&ensp;Loading...';
-        donateButton.setAttribute("disabled", true);
-        donateButton.classList.add("disabled");
-        coverFeeButton.setAttribute("disabled", true);
-        coverFeeButton.classList.add("disabled");
-
-        fetch("/api/donate", {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            host: document.location.origin,
-            amount: amount
-          }),
-        })
-          .then(function (response) {
-            return response.json();
-          })
-          .then(function (session) {
-            return stripe.redirectToCheckout({ sessionId: session.id });
-          })
-          .then(function (result) {
-            if (result.error) {
-              alert(result.error.message);
-            }
-          })
-          .catch(function (error) {
-            console.error("Error:", error);
-          });
-      });
-  });
+  fetch("/api/donate", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      host: document.location.origin,
+      amount,
+      token,
+    }),
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (session) {
+      return stripe.redirectToCheckout({ sessionId: session.id });
+    })
+    .then(function (result) {
+      if (result.error) {
+        alert(result.error.message);
+      }
+    })
+    .catch(function (error) {
+      console.error("Error:", error);
+    });
 };
